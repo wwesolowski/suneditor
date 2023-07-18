bad91a850a053888f5e5392df7eb3fc5c17efe6f
wojciech.wesolowski <wwesolowski97@gmail.com>
2023-07-18 12:27:00 +0200

    Mention fix

--- src/plugins/index.d.ts
+++ src/plugins/index.d.ts
@@ -25,6 +25,7 @@ import image from "./dialog/image";
 import video from "./dialog/video";
 import audio from "./dialog/audio";
 import math from "./dialog/math";
+import mention from "./dialog/mention"
 
 // file browser
 import { FileBrowserPlugin } from "./FileBrowserPlugin";
